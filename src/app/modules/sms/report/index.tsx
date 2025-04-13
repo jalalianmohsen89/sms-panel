b7961188c9d79562a215ce20239c722a7fbaa221
Mohsen Jalalian <50502137+jalalianmohsen89@users.noreply.github.com>
2025-04-13 17:39:39 +0330

    update report page

--- src/app/modules/sms/report/index.tsx
+++ src/app/modules/sms/report/index.tsx
@@ -1,7 +1,7 @@
 import { PageBuilder } from "@/core/components/composite";
 
 const Report = () => (
-  <PageBuilder pageId="sms" withPagination skipUrlParams={false} />
+  <PageBuilder pageId="sms" withPagination={false} skipUrlParams={false} />
 );
 
 export default Report;
